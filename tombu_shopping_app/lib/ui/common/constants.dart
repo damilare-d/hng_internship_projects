44e22a5b3218ad79297431b58d734cf983e0960e
damilare-d <damilareakinwale0@gmail.com>
2024-07-09 21:43:36 +0100

    updated constants for api url

--- tombu_shopping_app/lib/ui/common/constants.dart
+++ tombu_shopping_app/lib/ui/common/constants.dart
@@ -1,7 +1,4 @@
-const imageUrl = "https://api.timbu.cloud/images/";
-const kOrganizationId = "4f0a4e3def204d408ed7086ac594092e";
-const kAppId = "OH35TILK1CDNBBX";
-const kApiKey = "fdab2dbd5c3142078d6833392bfa666920240707160136587933";
-
-//organization_id=4f0a4e3def204d408ed7086ac594092e&reverse_sort=false&page=1&size=10&Appid=OH35TILK1CDNBBX&Apikey=fdab2dbd5c3142078d6833392bfa666920240707160136587933",
-//
+const String imageUrl = "https://api.timbu.cloud/images/";
+const String kOrganizationId = "4f0a4e3def204d408ed7086ac594092e";
+const String kAppId = "OH35TILK1CDNBBX";
+const String kApiKey = "fdab2dbd5c3142078d6833392bfa666920240707160136587933";
