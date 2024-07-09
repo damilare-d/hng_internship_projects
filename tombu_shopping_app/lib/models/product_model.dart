9de710411fcb55a970704fb00acc165fa70422d8
damilare-d <damilareakinwale0@gmail.com>
2024-07-09 21:44:03 +0100

    updated models to get the right response from api

--- tombu_shopping_app/lib/models/product_model.dart
+++ tombu_shopping_app/lib/models/product_model.dart
@@ -2,32 +2,39 @@ import 'package:tombu_shopping_app/models/photo_model.dart';
 
 class Product {
   final String name;
-  final String description;
+  final String? description;
   final List<Photo> photos;
-  final double currentPrice;
+  final double? currentPrice;
+  final double? availableQuantity;
+  final String? dateCreated;
+  final double? buyingPrice;
+  final double? sellingPrice;
 
   Product({
     required this.name,
-    required this.description,
+    this.description,
     required this.photos,
-    required this.currentPrice,
+    this.currentPrice,
+    this.availableQuantity,
+    this.dateCreated,
+    this.buyingPrice,
+    this.sellingPrice,
   });
 
   factory Product.fromJson(Map<String, dynamic> json) {
     return Product(
       name: json['name'] ?? '',
-      description: json['description'] ?? '',
-      photos: json['photos'] == null
-          ? []
-          : List<Photo>.from(json["photos"]!.map((x) => Photo.fromJson(x))),
-      currentPrice: json['current_price'][0]['NGN'][0] ?? 0.0,
+      description: json['description'],
+      photos: json['photos'] != null
+          ? List<Photo>.from(json['photos'].map((x) => Photo.fromJson(x)))
+          : [],
+      currentPrice: json['current_price'] != null && json['current_price'].isNotEmpty
+          ? (json['current_price'][0]['NGN'][0] as double?)
+          : null,
+      availableQuantity: json['available_quantity'],
+      dateCreated: json['date_created'],
+      buyingPrice: json['buying_price'],
+      sellingPrice: json['selling_price'],
     );
   }
-
-  // String get imageUrl {
-  //   if (photos.isNotEmpty && photos[0] != null && photos[0]['url'] != null) {
-  //     return "https://api.timbu.cloud/images/" + photos[0]['url'];
-  //   }
-  //   return 'assets/images/empty_img_placeholders.jpg';
-  // }
 }
