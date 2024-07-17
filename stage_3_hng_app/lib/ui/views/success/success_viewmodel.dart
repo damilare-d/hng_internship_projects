1669b31533de58e316442cf86c1e620a4ebd58ae
damilare-d <damilareakinwale0@gmail.com>
2024-07-17 18:54:11 +0100

    updated navigation flow after success view

--- stage_3_hng_app/lib/ui/views/success/success_viewmodel.dart
+++ stage_3_hng_app/lib/ui/views/success/success_viewmodel.dart
@@ -2,11 +2,23 @@ import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 import 'package:stage_3_hng_app/app/app.locator.dart';
 import 'package:stage_3_hng_app/app/app.router.dart';
+import 'package:stage_3_hng_app/services/app_service.dart';
+import 'package:stage_3_hng_app/services/cart_service_service.dart';
 
-class SuccessViewModel extends BaseViewModel {
+class SuccessViewModel extends ReactiveViewModel {
   final _navigationService = locator<NavigationService>();
+  final _cartService = locator<CartServiceService>();
+  final _appService = locator<AppService>();
 
- void onTapOk() {
-   _navigationService.navigateTo(Routes.dashboardView);
- }
+  void Function(int value) get onItemTapped => _appService.onItemTapped;
+
+  void onTapOk() {
+    _cartService.clearItems();
+    _appService.onItemTapped(0);
+    _navigationService.navigateTo(Routes.dashboardView);
+  }
+
+  @override
+  // TODO: implement listenableServices
+  List<ListenableServiceMixin> get listenableServices => [_appService];
 }
