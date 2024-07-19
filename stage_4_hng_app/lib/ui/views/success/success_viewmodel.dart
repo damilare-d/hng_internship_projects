5a2460ca75f6b1c53137e467c0c193379509cf94
damilare-d <damilareakinwale0@gmail.com>
2024-07-19 06:05:55 +0100

    update order list from sucees view model

--- stage_4_hng_app/lib/ui/views/success/success_viewmodel.dart
+++ stage_4_hng_app/lib/ui/views/success/success_viewmodel.dart
@@ -4,22 +4,29 @@ import 'package:stage_4_hng_app/app/app.locator.dart';
 import 'package:stage_4_hng_app/app/app.router.dart';
 import 'package:stage_4_hng_app/services/app_service.dart';
 import 'package:stage_4_hng_app/services/cart_service_service.dart';
-
+import 'package:stage_4_hng_app/services/checkout_service.dart';
+import 'package:stage_4_hng_app/services/order_service.dart';
 
 class SuccessViewModel extends ReactiveViewModel {
   final _navigationService = locator<NavigationService>();
   final _cartService = locator<CartServiceService>();
   final _appService = locator<AppService>();
+  final _orderService = locator<OrderService>();
+  final _checkoutService = locator<CheckoutService>();
 
   void Function(int value) get onItemTapped => _appService.onItemTapped;
 
+  void addItemToOrderList() {
+    _orderService.addOrders(_checkoutService.checkoutItems);
+  }
+
   void onTapOk() {
+    addItemToOrderList();
     _cartService.clearItems();
     _appService.onItemTapped(0);
     _navigationService.navigateTo(Routes.dashboardView);
   }
 
   @override
-  // TODO: implement listenableServices
   List<ListenableServiceMixin> get listenableServices => [_appService];
 }
