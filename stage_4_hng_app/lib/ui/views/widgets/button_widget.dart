215475ec1d2ccd420e588dc382b0314644ae0e2d
damilare-d <damilareakinwale0@gmail.com>
2024-07-20 18:13:02 +0100

    created a button widget for reuse

---
+++ stage_4_hng_app/lib/ui/views/widgets/button_widget.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class ButtonWidget extends StatelessWidget {
+  final double? width;
+  final void Function()? onTap;
+  final double? height;
+  final IconData? icon;
+  final String buttonText;
+
+  const ButtonWidget(
+      {super.key,
+      this.width,
+      this.height,
+      this.icon,
+      required this.buttonText,
+      this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: width ?? double.maxFinite,
+        height: height ?? 35,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: const Color(0xff0072C6),
+        ),
+        child: Row(
+          children: [
+            if (icon != null) Icon(icon),
+            Text(
+              buttonText,
+              style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                  fontWeight: FontWeight.w500),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
